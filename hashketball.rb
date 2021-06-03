66d6c7e9f8fcc2a6f2ef6741357713d759d5e09c
Jonathan Yu <jonathan.yu.jwy2@yale.edu>
2021-06-03 15:06:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ require 'pry'
 def game_hash()
 
 
-{:home{:team_name => "Lakers", :colors => [blue, yellow], :players => [{}, {}, {}]},
+#{:home{:team_name => "Lakers", :colors => [blue, yellow], :players => [{}, {}, {}]},
  :away{:team_name, :colors, :players}}
 
 
